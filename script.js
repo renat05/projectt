638b7a06630c1b4b8ed09e824f6faaeb083691a7
renat <renat.safiyev@mail.ru>
2020-01-11 14:04:03 +0400

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+// let abc = {
+//     name: "dsd",
+//     age: 23,
+//     asd:false
+// };
+// console.log(abc['name']);
+
+let e = prompt('есть ли 18?', 'да');
+console.log(e); 
+// let arr = ['car','plane','taxi'];
+// console.log(arr[2]);
+
+
