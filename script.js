ab05f5c65b7dc1fff4e42c598385187ad823170e
renat <renat.safiyev@mail.ru>
2020-01-12 16:38:23 +0400

    change

--- script.js
+++ script.js
@@ -5,9 +5,30 @@
 // };
 // console.log(abc['name']);
 
-let e = prompt('есть ли 18?', 'да');
-console.log(e); 
+// let e = prompt('есть ли 18?', 'да');
+// console.log(e); 
 // let arr = ['car','plane','taxi'];
 // console.log(arr[2]);
 
+// var money = "ваш бюджет на месяц";
+// var time = " YYYY-MM-DD";
+
+
+// var appData {
+// name
+// }:
+// var money = { name: "QWERTY" };
+
+let num = 50;
+
+if (num<60) {
+    console.log("true")
+} else {
+    console.log("false")
+}
+
+while (num < 70) {
+    console.log(num);
+    num ++
+};
 
